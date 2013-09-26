c923b50b8e5d21ed8b3880813ccc4fc4da4804ac
StaticGa <staticga@gmail.com>
2013-09-26 16:04:16 +0800

    init

---
+++ TestCocoaPods/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestCocoaPods
+//
+//  Created by Static Ga on 13-9-26.
+//  Copyright (c) 2013年 Static Ga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestCocoaPodsTests/TestCocoaPodsTests.m
@@ -0,0 +1,34 @@
+//
+//  TestCocoaPodsTests.m
+//  TestCocoaPodsTests
+//
+//  Created by Static Ga on 13-9-26.
+//  Copyright (c) 2013年 Static Ga. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestCocoaPodsTests : XCTestCase
+
+@end
+
+@implementation TestCocoaPodsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
